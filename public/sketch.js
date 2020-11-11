6e82564243414323af407e64d8c62937d5804320
MatteoRepetto <matteorepetto0@gmail.com>
2020-11-11 12:17:42 +0100

    added socket.io both to server and client

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,32 @@
+let socket = io();
+
+socket.on("connect", newConnection);
+
+function newConnection() {
+  console.log("your id: " + socket.id);
+}
+
 function preload(){
   // put preload code here
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
-  // put setup code here
+
+  background('purple');
 }
 
 function draw() {
+
+}
+
+function mouseMoved() {
   ellipse(mouseX, mouseY, 100);
+  // create the message
+  let message = {
+    x: mouseX,
+    y: mouseY,
+  };
+  //send to the server
+  socket.emit("mouse", message);
 }
